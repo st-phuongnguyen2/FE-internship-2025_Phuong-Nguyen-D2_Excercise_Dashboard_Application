eb6f69f4f22aef424bc8e7c702ec5098c594e2a2
st-phuongnguyen2 <phuong.nguyen2@supremetech.vn>
2025-04-27 14:17:00 +0700

    implement register page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/home-page/HomePage';
 import LoginPage from './pages/login-page/LoginPage';
+import RegisterPage from './pages/register-page/RegisterPage';
 import { AppRoutes } from './utils/constants/app-routes';
 
 export const router = createBrowserRouter([
   {
     path: AppRoutes.HOME,
     element: <HomePage />
+  },
+  {
+    path: AppRoutes.REGISTER,
+    element: <RegisterPage />
   },
   {
     path: AppRoutes.LOGIN,
