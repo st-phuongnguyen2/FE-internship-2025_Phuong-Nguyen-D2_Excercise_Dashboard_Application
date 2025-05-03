a2e0a912ab0063827b7ebc08b60fe69920580142
st-phuongnguyen2 <phuong.nguyen2@supremetech.vn>
2025-05-03 11:49:23 +0700

    rename app router file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { homepageRoutes } from './pages/homepage/homepage.routes';
 import { taskRoutes } from './pages/my-task/my-task.routes';
 
 const route = [...authRoutes, ...homepageRoutes, ...taskRoutes];
-console.log('route:', route);
 
 export const router = createBrowserRouter([
   {
